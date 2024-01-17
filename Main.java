38f9ce4ff477c1f9acb6cbcb97fbdbb3fe318f6e
Cedric <cedric.atzori@le-campus-numerique.fr>
2024-01-17 10:58:03 +0100

    Update main method for multiple moves in TicTacToe
    The main method in the Main.java file has been updated to handle multiple moves in the TicTacToe game. It not only initializes the TicTacToe game but also displays the game board, fetches the move from the player, and sets the owner. This is done twice for two subsequent moves in the game.

--- Main.java
+++ Main.java
@@ -1,7 +1,24 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args){
-        TicTacToe ticTacToe = new TicTacToe(3);
+        TicTacToe ticTacToe = new TicTacToe();
+
         ticTacToe.display();
+
+        int[] move = ticTacToe.getMoveFromPlayer();
+        System.out.println("Le joueur a choisi la ligne " + move[0] + " et la colonne " + move[1]);
+        ticTacToe.setOwner(move[0], move[1], ticTacToe.player);
+
+        ticTacToe.display();
+
+        int[] move1 = ticTacToe.getMoveFromPlayer();
+        System.out.println("Le joueur a choisi la ligne " + move1[0] + " et la colonne " + move1[1]);
+        ticTacToe.setOwner(move1[0], move1[1], ticTacToe.player);
+
+        ticTacToe.display();
+
+
     }
 
 }
