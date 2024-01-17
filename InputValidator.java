8e54e30379f070aef37d82c0336167afe83b70da
Cedric <cedric.atzori@le-campus-numerique.fr>
2024-01-17 09:09:59 +0100

    Add InputValidator class for number validation
    This commit introduces a new class, InputValidator, designed to validate user input. It currently supports validation for numbers in the range 0-2 utilizing regular expressions.

---
+++ InputValidator.java
@@ -0,0 +1,12 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputValidator {
+    private static final String VALID_NUMBER_REGEX = "^[0-2]+$";
+    private final Pattern pattern = Pattern.compile(VALID_NUMBER_REGEX);
+
+    public boolean isValid(String userInput) {
+        Matcher matcher = pattern.matcher(userInput);
+        return matcher.matches();
+    }
+}
