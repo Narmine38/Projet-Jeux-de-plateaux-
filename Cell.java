d49e2ffb3b9a499ee91e2d65e1ac957e04f247ce
Cedric <cedric.atzori@le-campus-numerique.fr>
2024-01-17 14:05:14 +0100

    Updated 'Cell' class methods and variables
    Refactored 'Cell' class in TicTacToe and removed commented-out code. The 'getRepresentation' method now uses a private variable 'representation'. Additionally, the 'setOwner' method is updated for better code readability by changing its parameter from 'owner' to 'player'.

--- Cell.java
+++ Cell.java
@@ -1,17 +1,13 @@
 public class Cell {
 
-//    public String getRepresentation() {
-//        return "|   ";
-//    }
-
     private String representation = "|   ";
 
     public String getRepresentation() {
         return representation;
     }
 
-    public void setOwner(Player owner) {
-        this.representation = owner.getRepresentation();
+    public void setOwner(Player player) {
+        this.representation = player.getRepresentation();
     }
 
 }
