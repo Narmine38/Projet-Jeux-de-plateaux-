b82729405e6db7f85a8b235fdd9380dcad963c50
Cedric <cedric.atzori@le-campus-numerique.fr>
2024-01-17 10:59:35 +0100

    Refactor TicTacToe class with UserInput and Player capabilities
    The TicTacToe class has been enhanced with UserInput and Player instances. A method "getMoveFromPlayer" is added to allow players to input their moves. The size of the game board has been fixed at 3, removing the size parameter from the class's constructor.

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,43 +1,53 @@
+import java.util.Scanner;
+
 public class TicTacToe {
 
-    // La taille du plateau de jeu (carré)
-    final int SIZE;
-
-    // Tableau représentant les cellules du plateau
+    private final UserInput userInput = new UserInput();
+    private String userChoice() {
+        return userInput.getValidatedInput();
+    }
+    final Player player = new Player("| O ");
+    private final int SIZE;
     Cell[] board;
 
-    // Constructeur pour initialiser le jeu avec une taille spécifique
-    public TicTacToe(int size) {
-        this.SIZE = size;
+    public TicTacToe() {
+        this.SIZE = 3;
         this.board = new Cell[SIZE * SIZE];
-
-        // Initialisation de chaque cellule du plateau
         for (int i = 0; i < SIZE * SIZE; i++) {
             this.board[i] = new Cell();
         }
     }
 
-    // Méthode pour afficher le plateau de jeu
     public void display() {
-
-        // Boucle pour parcourir chaque ligne du plateau
         for (int i = 0; i < SIZE; i++) {
-
-            // Affiche une ligne de séparation entre chaque ligne du plateau
             System.out.println("- - - - - - -");
-
-            // Boucle pour parcourir chaque colonne de la ligne actuelle
             for (int j = 0; j < SIZE; j++) {
-
-                // Affiche la représentation de la cellule à la position i, j
                 System.out.print(board[i * SIZE + j].getRepresentation());
             }
-
-            // Saute une ligne après l'affichage de chaque ligne du plateau
             System.out.println("|");
         }
-
-        // Affiche une ligne de séparation après avoir affiché toutes les lignes du plateau
         System.out.println("- - - - - - -");
     }
+
+    public int[] getMoveFromPlayer() {
+        int row = askPlayerForCoordinate("row");
+        int column = askPlayerForCoordinate("column");
+        if (!"|   ".equals(board[row * SIZE + column].getRepresentation())) {
+            System.out.println(" La cellule choisie est déjà occupée. Réessayez.");
+            getMoveFromPlayer();
+        }
+        return new int[]{row, column};
+    }
+
+    private int askPlayerForCoordinate(String type) {
+        System.out.println(player.getRepresentation() + ", veuillez entrer votre choix " + type + ": ");
+        String input = userChoice();
+        return Integer.parseInt(input);
+    }
+
+    public void setOwner(int row, int column, Player player) {
+        board[row * SIZE + column].setOwner(player);
+    }
 }
+
+
