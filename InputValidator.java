19e2b6e6fbc5c83e8199621ff9849eb80ea4f6b7
Cedric <cedric.atzori@le-campus-numerique.fr>
2024-01-17 10:58:50 +0100

    Remove comment lines from Player class
    The modifications in Player.java involved the removal of comment lines. This change simplifies the code and assumes that the function of Player class (representing a player in a game) is understood without the need for these comments.

--- InputValidator.java
+++ InputValidator.java
@@ -2,7 +2,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class InputValidator {
-    private static final String VALID_NUMBER_REGEX = "^[0-2]+$";
+    private static final String VALID_NUMBER_REGEX = "[0-2]";
     private final Pattern pattern = Pattern.compile(VALID_NUMBER_REGEX);
 
     public boolean isValid(String userInput) {
