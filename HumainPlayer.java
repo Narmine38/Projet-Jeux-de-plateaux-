5f618abb4fb2df62ed5d53cca0c0f3341b16555b
Cedric <cedric.atzori@le-campus-numerique.fr>
2024-01-19 14:44:26 +0100

    The commit message provided doesn't match with the provided diff.
    Here is the correct commit message:
    
    Add HumainPlayer.java extending Player
    
    This commit adds a new class, HumainPlayer.java, which extends the Player class. The class contains constructor that initializes the player's representation. It validates the representation to ensure it's either "X" or "O" for a game scenario, throwing an IllegalArgumentException if the representation doesn't match with the required criteria.

---
+++ HumainPlayer.java
@@ -0,0 +1,14 @@
+public class HumainPlayer extends Player {
+
+    /**
+     * Constructeur du joueur qui initialise sa représentation.
+     *
+     * @param representation la représentation du joueur
+     * @throws IllegalArgumentException si la représentation du joueur n'est ni "X" ni "O"
+     */
+    public HumainPlayer(String representation) {
+        super(representation);
+    }
+
+
+}
