5193f338ae79901552ce1bfdf2a4505d1101e2f1
Cedric <cedric.atzori@le-campus-numerique.fr>
2024-01-19 14:41:04 +0100

    Update PlayerManager, add game type selection
    PlayerManager has been enhanced to allow for game type selection. Now, players can choose between playing with another human, with the computer, or having two computers play against each other. This significantly improves the versatility of the game.

--- PlayerManager.java
+++ PlayerManager.java
@@ -1,28 +1,42 @@
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  * La classe PlayerManager gère la création et la gestion des joueurs.
  */
 public class PlayerManager {
-    // Création d'un joueur humain avec le symbol "O"
-    private final Player player = new Player("O");
-
-    // Création d'un joueur ordinateur avec le symbol "X"
-    private final Player computer = new Player("X");
-
+    private Player player1;
+    private Player player2;
     // Référence vers le joueur courant
     private Player currentPlayer;
 
-    /**
-     * Le constructeur de PlayerManager met le joueur humain comme joueur courant.
-     */
-    public PlayerManager() {
-        currentPlayer = player;
+    public void choiceGameType() {
+        int gameType = -1;
+        while (gameType != 0 && gameType != 1 && gameType != 2) {
+            View.selectedGameType();
+            gameType = Integer.parseInt(UserInput.getValidatedInput());
+            if (gameType == 0) {
+                player1 = new HumainPlayer("O");
+                player2 = new ArtificialPlayer("X");
+            } else if (gameType == 1) {
+                player1 = new HumainPlayer("O");
+                player2 = new HumainPlayer("X");
+            } else if(gameType == 2){
+                player1 = new ArtificialPlayer("O");
+                player2 = new ArtificialPlayer("X");
+            }else
+                View.noValideTypeGame();
+        }
+        currentPlayer = player1;
     }
+
 
     /**
      * Cette méthode retourne le joueur courant.
+     *
      * @return Le joueur courant.
      */
-    public Player getCurrentPlayer(){
+    public Player getCurrentPlayer() {
         return this.currentPlayer;
     }
 
@@ -31,10 +45,17 @@ public class PlayerManager {
      * Si le joueur courant est le joueur humain, on passe au joueur ordinateur et vice versa.
      */
     public void switchPlayer() {
-        if (currentPlayer == player) {
-            currentPlayer = computer;
+        if (currentPlayer == player1) {
+            currentPlayer = player2;
         } else {
-            currentPlayer = player;
+            currentPlayer = player1;
         }
     }
+
+    public int RandomPlay() {
+        Random rand = new Random();
+        return rand.nextInt(3);
+    }
+
+
 }
